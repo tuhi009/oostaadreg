f4cd2deee21d5a23e1b3287c2e60e63fe14a05fc
Tahmid <tuhi009@live.com>
2020-03-10 21:37:55 +0600

    remove cors

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('access-control-allow-credentials: true');
+//header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 /**
  * Laravel - A PHP Framework For Web Artisans
